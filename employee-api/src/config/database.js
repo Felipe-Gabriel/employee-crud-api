16fb6b1dec895f9e0aa64f82e959a989240c5e6b
Felipe-Gabriel <felipegabriel73@hotmail.com>
2021-10-03 22:08:05 -0300

    finalizacao da API

--- employee-api/src/config/database.js
+++ employee-api/src/config/database.js
@@ -5,26 +5,25 @@
  * author: Felipe Gabriel
  */
 
-const { Pool } = require('pg');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-//==> conexão com a base de dados
-
-const pool = new Pool ({
-    connectionString: process.env.DATABASE_URL
-});
-
-pool.on('error', (err, client) => {
-    console.log('Unexpected error on idle client', err)
-    process.exit(-1);
-})
-
-pool.on('connect', ()=>{
-    console.log('Base de Dados conectado com sucesso!')
-});
-
-module.exports = {
-    query: (text, params) => pool.query(text, params),
-}
+ const { Pool } = require('pg');
+ const dotenv = require('dotenv');
+ 
+ dotenv.config();
+ 
+ // ==> Conexão com a Base de Dados:
+ const pool = new Pool({
+   connectionString: process.env.DATABASE_URL
+ });
+ 
+ pool.on('error', (err, client) => {
+   console.log('Unexpected error on idle client', err)
+   process.exit(-1);
+ });
+ 
+ pool.on('connect', () => {
+   console.log('Base de Dados conectado com sucesso!')
+ });
+ 
+ module.exports = {
+   query: (text, params) => pool.query(text, params),
+ };
